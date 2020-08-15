c55cc409b20dad7fd944a6b27dff5ffd48626926
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2020-08-15 12:14:56 +0200

    Fix vision.core

--- fastai2/vision/core.py
+++ fastai2/vision/core.py
@@ -8,7 +8,6 @@ __all__ = ['Image', 'ToTensor', 'imagenet_stats', 'cifar_stats', 'mnist_stats', 
 # Cell
 from ..torch_basics import *
 from ..data.all import *
-from ..data.external import *
 
 from PIL import Image
 
